d79e3cfca35abdb6b471b1012a5f114e92b33886
sclau <saftaclaudia27@yahoo.com>
2025-06-29 16:20:19 +0300

    add emailjs to handleSubmit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import emailjs from "emailjs-com";
 
 function Contact() {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+  const [succes, setSuccces] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -9,6 +13,21 @@ function Contact() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    emailjs
+      .send("service_yjtyup2", "template_u8zklev", form, "eyzRAnrj05vrYo--H")
+      .then((result) => {
+        console.log(result.text);
+        setSuccces(true);
+        setError(false);
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        console.error(error.text);
+        setError(true);
+        setSuccces(false);
+      });
+
     console.log("Submitted form", form);
     //send to email or backend
   };
@@ -19,7 +38,7 @@ function Contact() {
       className="py-20 px-6 bg-gradient-to-b from-gray-50 to-white"
     >
       <div className="max-w-2xl mx-auto">
-        <h2 className="text-3xl font-bold text-gray-800 text-center mb-64">
+        <h2 className="text-3xl font-bold text-gray-800 text-center mb-6">
           Contact
         </h2>
         <p className="text-gray-600 text-center mb-8">
@@ -28,52 +47,50 @@ function Contact() {
         </p>
 
         <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Name
-            </label>
-            <input
-              name="name"
-              type="text"
-              value={form.name}
-              onChange={handleChange}
-              required
-              className="mt-1 w-full rounded-md border border-gray-300 pc-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Email
-            </label>
-            <input
-              name="email"
-              type="email"
-              value={form.email}
-              onChange={handleChange}
-              required
-              className="mt-1 w-full rounded-md border border-gray-300 pc-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
+          <input
+            name="name"
+            type="text"
+            value={form.name}
+            onChange={handleChange}
+            placeholder="Your name"
+            required
+            className=" w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
+          />
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Message
-            </label>
-            <textarea
-              name="message"
-              rows="5"
-              value={form.message}
-              onChange={handleChange}
-              required
-              className="mt-1 w-full rounded-md border border-gray-300 pc-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
+          <input
+            name="email"
+            type="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Your email"
+            required
+            className="w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
+          />
+
+          <textarea
+            name="message"
+            rows="5"
+            value={form.message}
+            onChange={handleChange}
+            placeholder="Your message"
+            required
+            className=" w-full rounded-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
+          />
+
           <button
             type="submit"
-            className="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition"
+            className="bg-gray-600 text-white px-6 py-2 rounded-md hover:bg-gray-700 transition"
           >
             Send Message
           </button>
+          {succes && (
+            <p className="text-green-600">Message sent successfully</p>
+          )}
+          {error && (
+            <p className="text-red-600">
+              Oops! Something went wrong.sent successfully
+            </p>
+          )}
         </form>
       </div>
     </section>
